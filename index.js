9b2af2347e242ad14f2fc42980f309f089f8cba7
Seunggyu <seunggyul14@icloud.com>
2022-11-30 07:59:59 -0800

    commit tree aa7595e0bea4ce7b99a0554bb271566fbd5e350d

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (num1 , num2) => num1 / num2;
+console.log(divide(2000, 100));
+
+const square = (num1) => num1 ** num1;
+console.log(square(2));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(3, 4));
